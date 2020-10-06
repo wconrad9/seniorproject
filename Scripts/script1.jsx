5a496aec9dad032524b00a07d783e4c1514dca47
Conrad <wconrad@AXN014-18346.local>
2020-10-06 09:03:48 -0400

    creating skeleton

---
+++ Scripts/script1.jsx
@@ -0,0 +1,8 @@
+var firstComp = app.project.item(1);
+//alert("number of layers is " + firstComp.numLayers);
+//alert("name of last layer is " + firstComp.layer(firstComp.numLayers).name);
+
+var newComp = app.project.items.addComp("Yay", 1920, 1080, 1, 10, 60.00);
+
+var moviepath = "~/Volumes/Workspaces/'Walter Conrad'"
+
